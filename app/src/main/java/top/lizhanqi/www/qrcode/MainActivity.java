c56d5cb4fd790b8a9899a9ac6be951bd052b0a11
lizhanqi <lizhanqihd@163.com>
2018-06-30 14:34:38 +0800

    init (二维码扫描,这个是熊宝贝员工的的提取)

---
+++ app/src/main/java/top/lizhanqi/www/qrcode/MainActivity.java
@@ -0,0 +1,34 @@
+package top.lizhanqi.www.qrcode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import top.lizhanqi.www.zxing.scanner.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ab(View view) {
+        startActivityForResult(new Intent(this,CaptureActivity.class),88);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+            if (data!=null){
+                String result = data.getStringExtra("result");
+                Toast.makeText(MainActivity.this,result,Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
